9883a4eee7c6ad79df3f94024da387592f4c098a
Mustafa UZUN <mustafauzun0@gmail.com>
2023-05-23 15:12:13 +0300

    Update metric.js

--- metric.js
+++ metric.js
@@ -1,98 +1,23 @@
 const convertMetric = (value, oldMetric, newMetric) => {
+    const getOldMetric = getMetricValue(oldMetric);
+    const getNewMetric = getMetricValue(newMetric);
+    
+    const difference = getOldMetric - getNewMetric;
 
-	const metric = {
+    return value * +`1e${difference}`;
 
-		Y: {
-			name: 'Yotta',
-			value: 24
-		},
-		Z: {
-			name: 'Zetta',
-			value: 21
-		},
-		E: {
-			name: 'Exa',
-			value: 18
-		},
-		P: {
-			name: 'Peta',
-			value: 15
-		},
-		T: {
-			name: 'Tera',
-			value: 12
-		},
-		G: {
-			name: 'Giga',
-			value: 9
-		},
-		M: {
-			name: 'Mega',
-			value: 6
-		},
-		k: {
-			name: 'Kilo',
-			value: 3
-		},
-		h: {
-			name: 'Hecto',
-			value: 2
-		},
-		da: {
-			name: 'Deca',
-			value: 1
-		},
-		'-': {
-			name: '-',
-			value: 0
-		},
-		d: {
-			name: 'Deci',
-			value: -1
-		},
-		c: {
-			name: 'Centi',
-			value: -2
-		},
-		m: {
-			name: 'Milli',
-			value: -3
-		},
-		u: {
-			name: 'Micro',
-			value: -6
-		},
-		n: {
-			name: 'Nano',
-			value: -9
-		},
-		p: {
-			name: 'Pico',
-			value: -12
-		},
-		f: {
-			name: 'Femto',
-			value: -15
-		},
-		a: {
-			name: 'Atto',
-			value: -18
-		},
-		z: {
-			name: 'Zepto',
-			value: -21
-		},
-		y: {
-			name: 'Yocto',
-			value: -24
-		}
+    function getMetricValue(metric) {
+        const metrics = ['Y', 'Z', 'E', 'P', 'T', 'G', 'M', 'k', 'h', 'da', '-', 'd', 'c', 'm', 'u', 'n', 'p', 'f', 'a', 'z', 'y'];
+        const select = metrics.indexOf('-') - metrics.indexOf(metric);
 
-	}
-
-	const difference = metric[oldMetric].value - metric[newMetric].value
-
-	return value * Number('1e' + difference)
-
+        if(Math.abs(select) > 3) {
+            const multiplier = select > 0 ? -2 : +2;
+            
+            return (select + multiplier) * 3;
+        }
+        
+        return select;
+    }
 }
 
 console.log(convertMetric(100, 'd', '-'))
